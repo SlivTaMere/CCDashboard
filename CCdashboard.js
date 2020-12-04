46d3f03612b57f473399f7da17f4087621ec62b2
STM <slivtamere@gmail.com>
2020-12-04 02:35:07 -0500

    readded 7d %

--- CCdashboard.js
+++ CCdashboard.js
@@ -98,13 +98,19 @@
             return e.toLowerCase() in r && (i = r[e.toLowerCase()] + i), a && (i = i + ' <span style="font-size:9px">' + e.toUpperCase() + "</span>"), i
         }
 
-        function l(e, t, a, i, l, s, d, p, m, u, h, g, v, f, x, y, b, w, L) {
+        function l(e, t, a, i, l, s, d, p, change24h, u, h, g, v, f, x, y, b, w, L, change7d) {
             var k = w ? "https://s2.coinmarketcap.com/static/img/coins/64x64/" + w + ".png" : "https://files.coinmarketcap.com/static/widget/coins_legacy/64x64/" + e + ".png",
                 C = "#009e73";
-            m < 0 && (C = "#d94040"), m = r.toLocaleString(m, {
+            change24h < 0 && (C = "#d94040"), change24h = r.toLocaleString(change24h, {
                 minDecimalPlaces: 2,
                 maxDecimalPlaces: 2
-            }), valTickerHTML = v ? "(" + a + ")" : "", valPrice = s ? o(s, i) : "?", valPercentHTML = m ? '<span style="color:' + C + '">(' + m + "%)" : "", valMarketCap = u ? n(u, 2) : "?", valVolume = h ? n(h, 2) : "?", poweredBy = "zh" == L ? "ç”±CoinMarketCapè£å¹¸å‘ˆçŽ°" : "Powered by CoinMarketCap", d ? (mainLineHeight = 25, valPriceSecondary = p ? o(p, d) : "?", secondaryHTML = '<br><span style="font-size: 12px; color: rgba(200, 200, 200, 1)">' + valPriceSecondary + " " + d + " </span>") : (mainLineHeight = 30, secondaryHTML = "");
+            }), 
+			C7d = "#009e73";
+			change7d < 0 && (C7d = "#d94040"), change7d = r.toLocaleString(change7d, {
+                minDecimalPlaces: 2,
+                maxDecimalPlaces: 2
+            }),
+			valTickerHTML = v ? "(" + a + ")" : "", valPrice = s ? o(s, i) : "?", valPercentHTML = change24h ? '<br/><span style="color:' + C + '">(' + change24h + '% 24h)</span><br/><span style="color:' + C7d + '">(' + change7d + '% 7d)' : "", valMarketCap = u ? n(u, 2) : "?", valVolume = h ? n(h, 2) : "?", poweredBy = "", d ? (mainLineHeight = 25, valPriceSecondary = p ? o(p, d) : "?", secondaryHTML = '<br><span style="font-size: 12px; color: rgba(200, 200, 200, 1)">' + valPriceSecondary + " " + d + " </span>") : (mainLineHeight = 30, secondaryHTML = "");
             var P = "utm_medium=widget&utm_campaign=cmcwidget&utm_source=" + location.hostname + "&utm_content=" + e,
                 M = '<div style="border:2px solid #e1e5ea;border-radius: 10px;font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif;min-width:285px;">    <div>        <div style="float:right;width:67%;border: none;text-align:left;padding:5px 0px;line-height:' + mainLineHeight + 'px;">            <span style="font-size: 18px;"><a href="https://coinmarketcap.com/currencies/' + e + "/?" + P + '" target="_blank">' + t + " " + valTickerHTML + '</a></span> <br>            <span style="font-size: 16px;">' + valPrice + " " + i + " " + valPercentHTML + "</span></span>            " + secondaryHTML + '        </div>        <div style="text-align:center;padding:5px 0px;width:33%;"><img src="' + k + '"></div>    </div>';
             return M += function(e, t, a, i, r, n, o, l, s) {
@@ -146,7 +152,8 @@
                             f = r && parseFloat(g.quote[r].price),
                             x = parseInt(g.quote[i].market_cap),
                             y = parseInt(g.quote[i].volume_24h),
-                            b = parseFloat(g.quote[i].percent_change_24h),
+                            change24h = parseFloat(g.quote[i].percent_change_24h),
+                            change7d = parseFloat(g.quote[i].percent_change_7d),
                             w = c[currencyid].name,
                             L = c[currencyid].symbol,
                             k = c[currencyid].cmc_rank;
@@ -155,9 +162,9 @@
                                 P = r ? "price_" + r.toLowerCase() : null,
                                 M = "market_cap_" + o.toLowerCase(),
                                 _ = "24h_volume_" + o.toLowerCase();
-                            v = parseFloat(c[C]), f = P ? parseFloat(c[P]) : null, x = parseInt(c[M]), y = parseInt(c[_]), b = Number(c.percent_change_24h), w = c.name, L = c.symbol, k = c.rank
+                            v = parseFloat(c[C]), f = P ? parseFloat(c[P]) : null, x = parseInt(c[M]), y = parseInt(c[_]), change24h = Number(c.percent_change_24h), w = c.name, L = c.symbol, k = c.rank
                         }
-                        var S = l(t, w, L, i, o, v, r, f, b, x, y, k, s, d, m, p, u, currencyid, n);
+                        var S = l(t, w, L, i, o, v, r, f, change24h, x, y, k, s, d, m, p, u, currencyid, n, change7d);
                         e(h).html(S), e(h).find("a").css({
                             "text-decoration": "none",
                             color: "#1070e0"
@@ -168,3 +175,4 @@
         })
     }
 }();
+
